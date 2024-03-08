d2b7cb048ff335882a5f8b8871cf86da4e313771
DESKTOP-JV3V0TT\dmelnez <csi23-dmelnez@colegioaltair.net>
2024-03-08 09:41:03 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio07.Repaso.C/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ejercicio07.Repaso.C
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("INTRODUCE UNA FRASE: ");
+            string fraseUsuario = Console.ReadLine();
+
+            string[] fraseDividad = fraseUsuario.Split(' ');
+            Console.WriteLine("LA FRASE TIENE UN TAMANIO: " + fraseDividad.Length);
+
+        }
+    }
+
+}
